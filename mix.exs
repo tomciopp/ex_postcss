6a289e2459e32a905fa2cd88aad58f4909edfb09
Thomas Cioppettini <544875+tomciopp@users.noreply.github.com>
2022-03-21 09:46:49 -0400

    chore(Docs): fix ex_doc configuration

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule ExPostcss.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, ">= 0.0.0", only: :docs}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
